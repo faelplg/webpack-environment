74df6b2828a60598ff312648216c44e8869f4a91
Rafael Goulart <faelplg@gmail.com>
2019-09-10 00:21:04 -0300

    Updating packages. Removing packages from development.

--- old-src/README.md
+++
@@ -1,7 +0,0 @@
-# My project
-
-## Headline 1
-This is text.
-
-## Headline 2
-This is text.
--- old-src/index.js
+++
@@ -1,4 +0,0 @@
-import readme from './README.md';
-import './style.css';
-
-console.log('This is the main script of the project src.');
